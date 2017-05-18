7db2249f9542d6da30c996b6c0eb68dd6e67c212
haryanawisnu <arya.wisnu05@gmail.com>
2017-05-18 14:34:59 +0700

    add server controller article (autentication update and showwithquery article)

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -12,12 +12,27 @@ let articleControllers = {
     });
   },
   showWithQuery: function(req, res) {
-    //_id: req.params.query
-    Article.find().exec((err, user) => {
+    let list = [];
+    let query = "",
+      value = req.params.value;
+    Article.find().populate('author').exec((err, result) => {
       if (err) {
         res.send(err);
       } else {
-        res.send(user);
+        if (req.params.query == "title") {
+          query = "title";
+        } else if (req.params.query == "category") {
+          query = "category";
+        } else if (req.params.query == "author") {
+          query = "authr._id";
+        }
+        result.forEach(data => {
+          if (data[query] == value) {
+            list.push(data);
+          }
+        })
+        console.log(query + "&" + value);
+        res.send(list);
       }
     });
   },
@@ -26,38 +41,57 @@ let articleControllers = {
     let iduser = req.params.iduser;
     Article.findOne({
       _id: idarticle
-    }).exec(function(err, result) {
+    }).populate('author').exec(function(err, result) {
       if (result) {
-        Article.update({
-          _id: idarticle
-        }, {
-          $set: {
-            title: req.body.title || result.title,
-            description: req.body.description || result.description,
-            category: req.body.category || result.category,
-            description: req.body.description || result.description
-          }
-        }, function(err, result) {
-          if (result) {
-            res.send(result);
-          } else {
-            res.send(err);
-          }
-        });
+        if (result.author._id == iduser) {
+          Article.update({
+            _id: idarticle
+          }, {
+            $set: {
+              title: req.body.title || result.title,
+              description: req.body.description || result.description,
+              category: req.body.category || result.category,
+              description: req.body.description || result.description
+            }
+          }, function(err, result) {
+            if (result) {
+              res.send({
+                err: false,
+                message: "Success Update",
+                data: result
+              });
+            } else {
+              res.send({
+                err: true,
+                message: "Err Update : " + err,
+                data: null
+              });
+            }
+          });
+        } else {
+          res.send({
+            err: true,
+            message: "Anda bukan author article ini",
+            data: null
+          });
+        }
       } else {
-        res.send(err);
+        res.send({
+          err: true,
+          message: "Article not found",
+          data: null
+        });
       }
     });
   },
   create: (req, res, next) => {
-    if (req.params.status) {
+    if (req.params.status == 'true') {
       Article.create({
         title: req.body.title,
         description: req.body.description,
         category: req.body.category,
-        description: req.body.description,
         author: req.body.author,
-        created: req.body.created
+        created: new Date
       }, function(err, result) {
         if (result) {
           res.send({
@@ -82,13 +116,40 @@ let articleControllers = {
     }
   },
   delete: function(req, res) {
-    Article.findByIdAndRemove(req.params.id, (err, user) => {
+    let idarticle = req.params.idarticle;
+    let iduser = req.params.iduser;
+    Article.findOne({
+      _id: idarticle
+    }).populate('author').exec((err, result) => {
       if (err) {
         res.send(err);
       } else {
-        res.send(user);
+        if (result.author._id == iduser) {
+          Article.findByIdAndRemove(idarticle, (err, artic) => {
+            if (err) {
+              res.send({
+                err: true,
+                message: "ERR Delete : " + err,
+                data: null
+              });
+            } else {
+              res.send({
+                err: false,
+                message: "Success Delete",
+                data: artic
+              });
+            }
+          });
+        } else {
+          res.send({
+            err: true,
+            message: "Anda bukan author article ini",
+            data: null
+          });
+        }
       }
     });
+
   }
 }
 
--- server/routes/article.js
+++ server/routes/article.js
@@ -3,7 +3,7 @@ var router = app.Router();
 var article = require('../controllers/article')
 
 router.get("/", article.showAll);
-router.get("/:query", article.showWithQuery);
+router.get("/:query/:value", article.showWithQuery);
 router.post("/:status", article.create);
 router.delete("/:idarticle/:iduser", article.delete);
 router.put("/:idarticle/:iduser", article.update);
